5b21ca43a63390610183f8ce9c7f88a3e059df94
Erickson <erick2109@gmail.com>
2021-03-14 13:19:31 -0400

    fix

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -27,30 +27,29 @@ class Api::V1::AuthController < ApplicationController
         "id":5,
         "nombre": "Zulia",
         "sorteos":[
-          {"id":26,"nombre":"Zulia12A","nombre_largo":"Triple Zulia A 12:45PM","horac_ls":"12:40","horac_d":""},
-          {"id":27,"nombre":"Zulia12B","nombre_largo":"Triple Zulia B 12:12PM","horac_ls":"12:40","horac_d":""},
-          {"id":28,"nombre":"Triptz-12","nombre_largo":"Zulia Tripletazo 12:45M","horac_ls":"12:40","horac_d":""},
-          {"id":29,"nombre":"Zulia 4A","nombre_largo":"Triple Zulia A 4:00PM","horac_ls":"16:40","horac_d":""},
-          {"id":30,"nombre":"Zulia 4B","nombre_largo":"Triple Zulia B 4:00PM","horac_ls":"16:40","horac_d":""},
-          {"id":31,"nombre":"Triptz-4","nombre_largo":"Zulia Tripletazo 4:00PM","horac_ls":"16:40","horac_d":""},
-          {"id":23,"nombre":"Zulia 7A","nombre_largo":"Triple Zulia A 7:00PM","horac_ls":"18:50","horac_d":"18:50"},
-          {"id":25,"nombre":"Zulia 7B","nombre_largo":"Triple Zulia A 7:00PM","horac_ls":"18:50","horac_d":"18:50"},
-          {"id":24,"nombre":"Triptz-7","nombre_largo":"Zulia tripletazo 7:00PM","horac_ls":"18:50","horac_d":"18:50"}
+          {"id":26,"nombre": "Zulia12A","nombre_largo": "Triple Zulia A 12:45PM","horac_ls": "12:40","horac_d": ""},
+          {"id":27,"nombre": "Zulia12B","nombre_largo": "Triple Zulia B 12:12PM","horac_ls": "12:40","horac_d": ""},
+          {"id":28,"nombre": "Triptz-12","nombre_largo": "Zulia Tripletazo 12:45M","horac_ls": "12:40","horac_d": ""},
+          {"id":29,"nombre": "Zulia 4A","nombre_largo": "Triple Zulia A 4:00PM","horac_ls": "16:40","horac_d": ""},
+          {"id":30,"nombre": "Zulia 4B","nombre_largo": "Triple Zulia B 4:00PM","horac_ls": "16:40","horac_d": ""},
+          {"id":31,"nombre": "Triptz-4","nombre_largo": "Zulia Tripletazo 4:00PM","horac_ls": "16:40","horac_d": ""},
+          {"id":23,"nombre": "Zulia 7A","nombre_largo": "Triple Zulia A 7:00PM","horac_ls": "18:50","horac_d": "18:50"},
+          {"id":25,"nombre": "Zulia 7B","nombre_largo": "Triple Zulia A 7:00PM","horac_ls": "18:50","horac_d": "18:50"},
+          {"id":24,"nombre": "Triptz-7","nombre_largo": "Zulia tripletazo 7:00PM","horac_ls": "18:50","horac_d": "18:50"}
         ]
       },
       {
         "id": 9,
         "nombre": "TripleZamorano",
         "sorteos": [
-          {"id"=>76, "nombre"=>"ZamoTzo4", "nombre_largo"=>"Zamorano Tripletazo 04:00PM", "horac_ls"=>"13:55", "horac_d"=>""},
-          {"id"=>81, "nombre"=>"ZamoTzo7", "nombre_largo"=>"Zamorano Tripletazo 07:00PM", "horac_ls"=>"18:55", "horac_d"=>"18:55"},
-          {"id"=>71, "nombre"=>"Tzamo-12A", "nombre_largo"=>"Triple Zaorano A 12:00M", "horac_ls"=>"11:55", "horac_d"=>nil},
-          {"id"=>73, "nombre"=>"ZamoTzo12", "nombre_largo"=>"Zamorano Tripletazo 12:00M", "horac_ls"=>"11:55", "horac_d"=>""},
-          {"id"=>74, "nombre"=>"Tzamo-4A", "nombre_largo"=>"Triple Zamorano A 04:00PM", "horac_ls"=>"13:55", "horac_d"=>nil},
-          {"id"=>77, "nombre"=>"Tzamo-7A", "nombre_largo"=>"Triple Zamorano A 07:00PM", "horac_ls"=>"18:55", "horac_d"=>"18:55"}
+          {"id": 76, "nombre": "ZamoTzo4", "nombre_largo": "Zamorano Tripletazo 04:00PM", "horac_ls": "13:55", "horac_d": ""},
+          {"id": 81, "nombre": "ZamoTzo7", "nombre_largo": "Zamorano Tripletazo 07:00PM", "horac_ls": "18:55", "horac_d": "18:55"},
+          {"id": 71, "nombre": "Tzamo-12A", "nombre_largo": "Triple Zaorano A 12:00M", "horac_ls": "11:55", "horac_d": nil},
+          {"id": 73, "nombre": "ZamoTzo12", "nombre_largo": "Zamorano Tripletazo 12:00M", "horac_ls": "11:55", "horac_d": ""},
+          {"id": 74, "nombre": "Tzamo-4A", "nombre_largo": "Triple Zamorano A 04:00PM", "horac_ls": "13:55", "horac_d": nil},
+          {"id": 77, "nombre": "Tzamo-7A", "nombre_largo": "Triple Zamorano A 07:00PM", "horac_ls": "18:55", "horac_d": "18:55"}
         ]
       }
-
     ]
   end
 
